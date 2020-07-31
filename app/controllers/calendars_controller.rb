98884fc247c833564738b7265a4c7dda9d672e41
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-07-31 18:14:52 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -23,18 +24,22 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例)  今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
 
     @plans = Plan.where(date: @todays_date..@todays_date + 7)
 
+    # 曜日を１週間分、一覧表示する 「+ x」を付属させることで１週間分の表示を可能にする
     7.times do |x|
       plans = []
+      # mapはeachの様な働きをするメソッド
       plan = @plans.map do |plan|
+        #pushを用いることで、配列plans[]にplanカラム(黄色)のplan(水色)を代入している
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans}
+    # ビューファイルに渡す値はここでハッシュを用いて定義されている
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => plans, :youbi => wdays[(@todays_date + x).wday] }
       @week_days.push(days)
     end
 
